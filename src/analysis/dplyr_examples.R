e97d107a2bf79fe8e91d5350d65dae24758874e1
keckstrom <korin.eckstrom@gmail.com>
2019-04-02 17:06:21 +0000

    Summarize gampinder and create plot.

---
+++ src/analysis/dplyr_examples.R
@@ -0,0 +1,68 @@
+# working with dpylr using the gapminder dataset
+library(tidyverse)
+gapminder <- readr::read_csv(here("data/gapminder/raw/gapminder_data.csv"))
+
+mean(gapminder$gdpPercap[gapminder$continent == "Africa"])
+mean(gapminder$gdpPercap[gapminder$continent == "Americas"])
+
+
+year_country_gdp <- select(gapminder, year, country, gdpPercap)
+
+# using pipe instead
+year_country_gdp <- gapminder %>%
+                    filter(continent == "Europe") %>%
+                    select(year, country, gdpPercap, continent)
+head(year_country_gdp)                    
+
+
+### produce dataframe of African life, country, year
+
+subset_africa <- gapminder %>%
+                filter(continent == "Africa") %>%
+                select(year, lifeExp, country)
+head(subset_africa)
+dim(subset_africa)
+
+
+### grouping data 
+gapminder %>%
+    group_by(continent) %>%
+    summarize(mean_val = mean(gdpPercap))
+
+# calc avglifeexp per country
+
+mean_lifeExp <- gapminder %>%
+    group_by(country) %>%
+    summarize(mean_val = mean(lifeExp))
+##Iceland is highest mean life expectancy
+
+mean_lifeExp %>%
+    filter(mean_lifeExp == min(mean_lifeExp) | mean_lifeExp == max(mean_lifeExp))
+
+
+gapminder %>%
+  group_by(continent) %>%
+  summarize(mean_val = mean(gdpPercap),
+            sd_gdpPercap = sd(gdpPercap))
+
+## can keep building levels with the pipes
+
+# can also pipe directly into a plot
+
+# plotting all of it
+ggplot(data = gapminder, aes(x= year, y= lifeExp, color = continent)) + 
+  geom_line() + facet_wrap(~country)
+
+# filtering with dpylr
+
+gapminder %>%
+  filter(continent == "Africa") %>%
+  ggplot( aes(x= year, y= lifeExp)) + 
+  geom_line() + facet_wrap(~country)
+
+
+
+
+
+
+
